ac80c392ea377f4afcdc1792075cbb3851b3d6f7
franck-benault <franck-benault@orange.fr>
2017-10-08 10:01:27 +0200

    add info end point testing

---
+++ 09EndPointsActuator/src/test/java/hello/health/InfoEndPointWithRandomPort.java
@@ -0,0 +1,37 @@
+package hello.health;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class InfoEndPointWithRandomPort {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+	@Test
+	public void testGreetingNoName() throws Exception {
+		
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/info",
+                String.class)).contains("my first spring boot application");
+		
+	}
+
+	
+
+
+}
