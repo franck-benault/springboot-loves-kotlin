ac620541f5f0e2e1648913249683c21515ed10af
franck-benault <franck-benault@orange.fr>
2017-10-08 15:49:55 +0200

    cleaning code remove unless import

--- 01SimpleProject/src/test/java/hello/ApplicationTest.java
+++ 01SimpleProject/src/test/java/hello/ApplicationTest.java
@@ -2,11 +2,7 @@ package hello;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.rule.OutputCapture;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
